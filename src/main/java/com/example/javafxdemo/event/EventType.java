f9190dc510d37e57d3dca933a53b306cc9ddac62
yudongwei <dongwei.yu@vevor.onaliyun.com>
2024-08-30 13:34:15 +0800

    adjust to different components

--- src/main/java/com/example/javafxdemo/event/EventType.java
+++ src/main/java/com/example/javafxdemo/event/EventType.java
@@ -5,8 +5,6 @@ public enum EventType {
     CONNECT_SUCCESS_CLUSTER,
     /**连接ES集群失败*/
     CONNECT_FAIL_CLUSTER,
-    /**拿到集群所有的索引*/
-    GET_CLUSTER_ALL_INDEX,
     /**根据指定的索引查数据*/
     QUERY_WITH_SPECIAL_INDEX,
     /**初始化集群对应的索引*/
